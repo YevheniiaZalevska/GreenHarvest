209c174444819ab507415cd2db21426b8a7ec449
YevheniiaZalevska <yevhenia05@gmail.com>
2025-02-28 08:01:00 +0100

    remove the btn to top

--- js/to-top.js
+++ js/to-top.js
@@ -1,19 +1,19 @@
-const buttonToTop = document.querySelector(".scroll-to-top");
+// const buttonToTop = document.querySelector(".scroll-to-top");
 
-buttonToTop.addEventListener("click", () => {
-  window.scrollTo({ top: 0, behavior: "smooth" });
-});
+// buttonToTop.addEventListener("click", () => {
+//   window.scrollTo({ top: 0, behavior: "smooth" });
+// });
 
-window.onscroll = () => changeScrollButtonVisibility();
+// window.onscroll = () => changeScrollButtonVisibility();
 
-function changeScrollButtonVisibility() {
-  const button = document.querySelector(".scroll-to-top");
-  const offsetTrigger = 60;
-  const pageOffset = window.pageYOffset;
+// function changeScrollButtonVisibility() {
+//   const button = document.querySelector(".scroll-to-top");
+//   const offsetTrigger = 60;
+//   const pageOffset = window.pageYOffset;
 
-  if (pageOffset > offsetTrigger) {
-    button.classList.remove("js-transparent");
-  } else {
-    button.classList.add("js-transparent");
-  }
-}
+//   if (pageOffset > offsetTrigger) {
+//     button.classList.remove("js-transparent");
+//   } else {
+//     button.classList.add("js-transparent");
+//   }
+// }
